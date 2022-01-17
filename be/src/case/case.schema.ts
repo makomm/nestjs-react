bb0a44aad4a6c472558fbee8f38e603088bdb5e6
Leonardo Dutra <leonardo.dutra@mindera.com>
2022-01-17 19:14:48 +0000

    define case schema

---
+++ be/src/case/case.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { User } from 'src/user/user.schema';
+import { Label } from 'src/label/label.schema';
+
+export type CaseDocument = Case & Document;
+
+@Schema()
+export class Case {
+  @Prop()
+  name: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User'})
+  user: User;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Label'})
+  label: Label;
+
+
+}
+
+export const CaseSchema = SchemaFactory.createForClass(Case);
