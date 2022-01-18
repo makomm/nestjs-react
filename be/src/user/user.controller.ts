db0e58151dabe964046f19afc8900d4c7b12d02e
Leonardo Dutra <leonardo.dutra@mindera.com>
2022-01-18 00:57:14 +0000

    create Readme.md

--- be/src/user/user.controller.ts
+++ be/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpException, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
